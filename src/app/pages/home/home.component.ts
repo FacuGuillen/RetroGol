2216b0f4c03da6ee95b49a5cd544113d97018b23
FacuGuillen <facu.guillen@hotmail.com>
2025-06-12 21:34:07 -0300

    Primer commit

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+  atleticomadridsuplente97 : string = 'imgs/equiposeuropa/atleticomadridsuplente97.jpg';
+  argentinasuplente94 : string = 'imgs/paises/argentinasuplente94.jpeg'
+  bocajuniors96 : string = 'imgs/futbolargentino/bocajuniors96.jpg'
+  botafogo92 : string = 'imgs/varios/botafogo92.jpg'
+}
